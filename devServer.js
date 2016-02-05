ee480a3a0732fbe394f7ed1686b1cecbc6c01bdb
Stephen Bunch <bunchclone@gmail.com>
2016-02-05 00:45:09 -0800

    Read PORT env

--- devServer.js
+++ devServer.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
-app.listen(9000, function(err) {
-  console.log('Server running at http://localhost:9000');
+
+var port = process.env.PORT;
+if (port) {
+  port = parseInt(port, 10);
+} else {
+  port = 9000;
+}
+app.listen(port, function(err) {
+  console.log(`Server running at http://localhost:${ port }`);
 });
